126536075151ad64afe1502afc725bd3d3229188
Matias0008 <mmatidelga2@gmail.com>
2023-06-19 20:59:54 -0300

    add lines for create tables

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from models.Estado_model import Estado
 
 if __name__ == "__main__":
     load_dotenv()
+    db = DatabaseController()
+    db.connect()
+    db.createTables()
     encuestaGestor = EncuestaController()
     encuestaPantalla = EncuestaBoundary(encuestaGestor)
     encuestaGestor.setPantalla(encuestaPantalla)
