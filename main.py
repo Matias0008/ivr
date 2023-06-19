9e46aada45b4e91b9a191b0e1623b875d0b90201
Matias0008 <mmatidelga2@gmail.com>
2023-06-19 20:08:38 -0300

    update

--- main.py
+++ main.py
@@ -6,12 +6,6 @@ from models.Estado_model import Estado
 
 if __name__ == "__main__":
     load_dotenv()
-    db = DatabaseController()
-    db.connect()
-    e1 = Estado(nombre = "Nombre", descripcion = "Descripcion", cambiosEstado = [])
-    db.session.add(e1)
-    db.session.commit()
-
     encuestaGestor = EncuestaController()
     encuestaPantalla = EncuestaBoundary(encuestaGestor)
     encuestaGestor.setPantalla(encuestaPantalla)
