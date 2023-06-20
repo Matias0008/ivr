30c3e30e92adf9bd253735bc4310e539a0e738eb
Matias0008 <mmatidelga2@gmail.com>
2023-06-19 21:19:34 -0300

    feat: tomarPeriodo() function

--- controllers/encuesta_controller.py
+++ controllers/encuesta_controller.py
@@ -8,6 +8,7 @@ class EncuestaController:
     pantalla: EncuestaBoundary
     fechaFin: str
     fechaInicio: str
+    llamadasDentroDePeriodo: List[Llamada]
 
     def setPantalla(self, pantalla: EncuestaBoundary):
         self.pantalla = pantalla
@@ -19,10 +20,11 @@ class EncuestaController:
         self.fechaInicio = fechaInicio
         self.fechaFin = fechaFin 
         self.buscarLlamadasDentroDePeriodo(self.fechaInicio, self.fechaFin)
-    
+
     def buscarLlamadasDentroDePeriodo(self, fechaInicio: str, fechaFin: str):
         db = DatabaseController()
         db.connect()
         llamadas: List[Llamada] = db.session.query(Llamada).all()
         for llamada in llamadas:
-            pass
+            if llamada.esDePeriodo(fechaInicio, fechaFin):
+                self.llamadasDentroDePeriodo.append(llamada)
