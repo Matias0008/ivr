0b1e3f4ce0297e49404606129a918168535d994b
Matias0008 <mmatidelga2@gmail.com>
2023-06-19 21:20:56 -0300

    feat: add esDePeriodo() function

--- models/Llamada_model.py
+++ models/Llamada_model.py
@@ -3,7 +3,6 @@ from sqlalchemy import Column, Integer, String, Boolean, ForeignKey
 from sqlalchemy.orm import declarative_base, relationship
 
 from models.Base import base
-from models.CambioEstado_model import CambioEstado
 
 class Llamada(base):
     __tablename__ = "llamada"
@@ -19,22 +18,18 @@ class Llamada(base):
     cambiosEstado = relationship("CambioEstado", back_populates="llamada")
 
     def esDePeriodo(self, fechaInicio: String, fechaFin: String):
-        ultimoCambioEstado = self.cambiosEstado[-1]
-        fechaHoraInicio = ultimoCambioEstado.getFechaHoraInicio()
-        return fechaInicio <= fechaHoraInicio <= fechaFin
+        fechaCreacion = self.determinarEstadoInicial()
+        return fechaInicio <= fechaCreacion <= fechaFin
     
     def determinarEstadoInicial(self):
         fechaHoraInicioMenor = None
-        primerCambioEstado = None
 
         for cambioEstado in self.cambiosEstado:
             fechaHoraInicio = cambioEstado.getFechaHoraInicio()
             if fechaHoraInicioMenor is None or fechaHoraInicio < fechaHoraInicioMenor:
                 fechaHoraInicioMenor = fechaHoraInicio
-                primerCambioEstado = cambioEstado
 
-        estadoInicial = primerCambioEstado.getNombreEstado()
-        return estadoInicial
+        return fechaHoraInicioMenor
 
     def getDuracion(self):
         return self.duracion
